37c2d1027633870a3a58121e63e2b35db6991a44
Arvel04 <arvelkenne@gmail.com>
2024-11-11 15:41:17 +0000

    HYLASTIX update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,29 @@
-variable "resource_group_location" {
-  default     = "West Europe"
-  description = "Location of the resource group."
+variable "resource_group_name" {
+  type    = string
+  default = "keycloak-terraform-rg"
+}
+
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1ms"
+}
+
+variable "vm_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "vm_password" {
+  type    = string
+  default = "Password123!"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "keycloak-vm"
 }
