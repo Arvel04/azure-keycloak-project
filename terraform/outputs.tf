ab5a1d0d1519cba6a0ec236558da3dc40940b832
Arvel04 <arvelkenne@gmail.com>
2024-11-12 15:26:01 +0000

    Hylastix0015

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,7 +4,6 @@ output "resource_group_name" {
 
 output "public_ip_address" {
   value = azurerm_public_ip.my_terraform_public_ip.ip_address
-  description = "Adresse IP publique de la VM"
 }
 
 output "tls_private_key" {
