f697d6600e88568a1e241f304f2703afcc414ef2
Arvel04 <arvelkenne@gmail.com>
2024-11-12 23:10:52 +0000

    Update24

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "resource_group_name" {
 }
 
 output "VM_IP" {
-  value = azurerm_public_ip.my_public_ip.ip_address
+  value = azurerm_public_ip.my_terraform_public_ip.ip_address
   description = "The public IP address of the VM"
 }
 
